0aae7677ab75b6ea0d68aad2f6d3d3a209d8b136
blcoccaro <blcoccaro@gmail.com>
2019-01-10 14:30:36 -0200

    presentation - partial

---
+++ v1/admin/event/presentation/list.php
@@ -0,0 +1,33 @@
+<?php
+    require_once($_SERVER['DOCUMENT_ROOT']."/v1/api_include.php");
+
+    function get($loggedId, $id_base, $id_evento) {
+        //sleep(5);
+        $query = "EXEC pr_admin_presentation_list ?";
+        //die("aaa.".print_r(db_param($startAt),true));
+        $params = array($id_evento);
+        $result = db_exec($query, $params, $id_base);
+
+        $json = array();
+        foreach ($result as &$row) {
+            $json[] = array(
+                "CodApresentacao" => $row["CodApresentacao"]
+                ,"weekday" => $row["weekday"]
+                ,"CodSala" => $row["CodSala"]
+                ,"NomSala" => $row["NomSala"]
+                ,"weekdayName" => $row["weekdayName"]
+                ,"DatApresentacao" => $row["DatApresentacao"]
+                ,"HorSessao" => $row["HorSessao"]
+                ,"ValPeca" => $row["ValPeca"]
+                ,"StaAtivoWeb" => $row["StaAtivoWeb"]
+                ,"StaAtivoBilheteria" => $row["StaAtivoBilheteria"]
+            );
+        }
+
+        echo json_encode($json);
+        logme();
+        die();    
+    }
+
+get($_REQUEST["loggedId"],$_REQUEST["id_base"],$_REQUEST["id_evento"]);
+?>
--- v1/admin/event/save.php
+++ v1/admin/event/save.php
@@ -1,30 +1,36 @@
 <?php
     require_once($_SERVER['DOCUMENT_ROOT']."/v1/api_include.php");
+    require_once($_SERVER['DOCUMENT_ROOT']."/lib/php-image-resize/ImageResize.php");
 
-    function execute($api
-        ,$id_base
-        ,$id_produtor
-        ,$id_to_admin_user
-        ,$CodPeca
-        ,$NomPeca
-        ,$CodTipPeca
-        ,$TemDurPeca
-        ,$CenPeca
-        ,$id_local_evento
-        ,$description
-        ,$meta_description
-        ,$meta_keyword
-        ,$opening_time
-        ,$insurance_policy
-        ,$showInBanner
-        ,$bannerDescription
-        ,$QtIngrPorPedido
-        ,$in_obriga_cpf
-        ,$qt_ingressos_por_cpf
+    use \Gumlet\ImageResize;
+    
+    function execute($apikey
+    ,$id_base
+    ,$id_produtor
+    ,$id_to_admin_user
+    ,$CodPeca
+    ,$NomPeca
+    ,$CodTipPeca
+    ,$TemDurPeca
+    ,$CenPeca
+    ,$id_local_evento
+    ,$description
+    ,$meta_description
+    ,$meta_keyword
+    ,$opening_time
+    ,$insurance_policy
+    ,$showInBanner
+    ,$bannerDescription
+    ,$QtIngrPorPedido
+    ,$in_obriga_cpf
+    ,$qt_ingressos_por_cpf
+    ,$imagechanged
+    ,$imagebase64
     ) {
+        //die("aqui: $imagechanged / $imagebase64");
         //sleep(5);
         $query = "EXEC pr_event_save ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?";
-        $params = array($api
+        $params = array($apikey
         ,$id_produtor
         ,$id_to_admin_user
         ,$CodPeca
@@ -33,28 +39,92 @@
         ,$TemDurPeca
         ,$CenPeca
         ,$id_local_evento
-        ,$ValIngresso
         ,$description
         ,$meta_description
         ,$meta_keyword
         ,$opening_time
         ,$insurance_policy
-        ,$showInBanner
+        ,$showInBanner == "" ? "0" : $showInBanner
         ,$bannerDescription
         ,$QtIngrPorPedido
-        ,$in_obriga_cpf
+        ,$in_obriga_cpf == "" ? "0" : $in_obriga_cpf
         ,$qt_ingressos_por_cpf);
+
+        //die("aqui".json_encode($params));
         $result = db_exec($query, $params, $id_base);
 
-        $json = array("success"=>true
+        $id_evento = 0;
+
+        foreach ($result as &$row) {
+            $id_evento = $row["id_evento"];
+        }
+
+        $json = array("success"=>($id_evento != 0)
         ,"msg"=>"Salvo com sucesso");
+
+        $imagelog = "";
+
+
+        if ($imagechanged || $imagechanged == 1 || $imagechanged == "1") {      
+            $imagelog = $imagelog."altered image|";
+            if (preg_match('/^data:image\/(\w+);base64,/', $imagebase64, $type)) {
+                $imagelog = $imagelog."recovering info|";
+                $imagebase64 = substr($imagebase64, strpos($imagebase64, ',') + 1);
+                $type = strtolower($type[1]); // jpg, png, gif
+                $img = base64_decode($imagebase64);
+                $imagelog = $imagelog."looking for directory: ".'/var/www/media/ori/'.$id_evento.'|';
+                if (!file_exists('/var/www/media/ori/'.$id_evento)) {
+                    $imagelog = $imagelog."folder not exist|";
+                    mkdir('/var/www/media/ori/'.$id_evento, 0777, true);
+                    $imagelog = $imagelog."created folder|";
+                }
+                $imagelog = $imagelog."looking for directory: ".'/var/www/media/evento/'.$id_evento.'|';
+                if (!file_exists('/var/www/media/evento/'.$id_evento)) {
+                    $imagelog = $imagelog."folder not exist|";
+                    mkdir('/var/www/media/evento/'.$id_evento, 0777, true);
+                    $imagelog = $imagelog."created folder|";
+                }
+                
+                $imagelog = $imagelog."looking for image: ".'/var/www/media/ori/'.$id_evento.'/'.getOriginalCardImageName().'|';
+                if (file_exists('/var/www/media/ori/'.$id_evento.'/'.getOriginalCardImageName())) {
+                    $imagelog = $imagelog."image exist|";
+                    unlink('/var/www/media/ori/'.$id_evento.'/'.getOriginalCardImageName());
+                    $imagelog = $imagelog."deleted image|";
+                }
+                if (file_exists('/var/www/media/evento/'.$id_evento.'/'.getDefaultCardImageName())) {
+                    $imagelog = $imagelog."image exist|";
+                    unlink('/var/www/media/evento/'.$id_evento.'/'.getDefaultCardImageName());
+                    $imagelog = $imagelog."deleted image|";
+                }
+                if (file_exists('/var/www/media/evento/'.$id_evento.'/'.getBigCardImageName())) {
+                    $imagelog = $imagelog."image exist|";
+                    unlink('/var/www/media/evento/'.$id_evento.'/'.getBigCardImageName());
+                    $imagelog = $imagelog."deleted image|";
+                }
+
+                $imagelog = $imagelog."saving|";
+                file_put_contents('/var/www/media/ori/'.$id_evento.'/'.getOriginalCardImageName(), $img);
+                $imagelog = $imagelog."saved";
+                //file_put_contents('/var/www/media/evento/'.$id_evento.'/'.getDefaultCardImageName(), $img);
+//                file_put_contents('/var/www/media/evento/'.$id_evento.'/'.getBigCardImageName(), $img);
+                                
+                $imageResizer = new ImageResize('/var/www/media/ori/'.$id_evento.'/'.getOriginalCardImageName());
+                $imageResizer->resize(600,314, $allow_enlarge = true);
+                $imageResizer->save('/var/www/media/evento/'.$id_evento.'/'.getDefaultCardImageName());
+
+                $imageResizer = new \Gumlet\ImageResize('/var/www/media/ori/'.$id_evento.'/'.getOriginalCardImageName());
+                $imageResizer->resize(816, 459, $allow_enlarge = true);
+                $imageResizer->save('/var/www/media/evento/'.$id_evento.'/'.getBigCardImageName());
+            }
+            $json["msg"] = $imagelog;
+        }
 
         echo json_encode($json);
         logme();
         die();    
     }
 
-execute($_REQUEST["api"]
+execute($_REQUEST["apikey"]
 ,$_POST["id_base"]
 ,$_POST["id_produtor"]
 ,$_POST["id_to_admin_user"]
@@ -64,7 +134,6 @@ execute($_REQUEST["api"]
 ,$_POST["TemDurPeca"]
 ,$_POST["CenPeca"]
 ,$_POST["id_local_evento"]
-,$_POST["ValIngresso"]
 ,$_POST["description"]
 ,$_POST["meta_description"]
 ,$_POST["meta_keyword"]
@@ -74,5 +143,7 @@ execute($_REQUEST["api"]
 ,$_POST["bannerDescription"]
 ,$_POST["QtIngrPorPedido"]
 ,$_POST["in_obriga_cpf"]
-,$_POST["qt_ingressos_por_cpf"]);
+,$_POST["qt_ingressos_por_cpf"]
+,$_POST["imagechanged"]
+,$_POST["imagebase64"]);
 ?>
