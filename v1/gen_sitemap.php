c57eb2ffc078f1eff34442d20a8a74aeb559e854
blcoccaro <blcoccaro@gmail.com>
2019-02-06 19:37:57 -0200

    sitemap

--- v1/gen_sitemap.php
+++ v1/gen_sitemap.php
@@ -1,9 +1,9 @@
 <?php
     require_once($_SERVER['DOCUMENT_ROOT']."/v1/api_include.php");
 
-    function get($api) {
+    function get($uniquename) {
         $query = "EXEC pr_generate_sitemap ?";
-        $params = array($api);
+        $params = array($uniquename);
         $result = db_exec($query, $params);
 
         $text = "";
@@ -17,8 +17,10 @@
 
         return $text;
     }
-    $myfile = fopen("/var/www/site/sitemap.xml", "w") or die("Unable to open file!");
-    $txt = get($_REQUEST["apikey"]);
+    $uniquename = $_REQUEST["uniquename"];
+    touch("/var/www/site/sitemap".$uniquename.".xml");
+    $myfile = fopen("/var/www/site/sitemap".$uniquename.".xml", "w") or die("Unable to open file!");
+    $txt = get($uniquename);
     fwrite($myfile, $txt);
     fclose($myfile);
     echo "generated sitemap.";
