0b4d9006dbcc3a83132c0acc379885d9e80553fa
Gabriel Craveiro <gabriel.craveiro@intuiti.com.br>
2019-02-13 10:00:28 -0200

    Rota para parcelamento

---
+++ v1/purchase/site/getinstallments.php
@@ -0,0 +1,25 @@
+
+<?php
+     require_once($_SERVER['DOCUMENT_ROOT']."/v1/api_include.php");
+     require_once($_SERVER['DOCUMENT_ROOT']."/v1/purchase/site/helper.php");
+     require_once($_SERVER['DOCUMENT_ROOT']."/v1/purchase/site/cardbin.php");
+     require_once($_SERVER['DOCUMENT_ROOT']."/v1/gateway/payment/pagarme.php");
+     require_once($_SERVER['DOCUMENT_ROOT']."/v1/purchase/site/session.php");
+     
+    function get($codCliente, $idSession) {
+      setsession($codCliente, $idSession);
+
+      $values = getvaluesofmyshoppig($codCliente);
+      $amount = $values[0]["totalwithservice"];
+      $id_purchase = get_id_purchase($id_session, $codCliente);
+
+      $installment_config = getinstallments($id_purchase, $codCliente);
+      $installment_gateway = pagarme_installments($installment_config["free_installments"], $installment_config["max_installments"], $installment_config["interest_rate"], $amount);
+      
+      echo json_encode($installment_gateway);
+        logme();
+        die();    
+    }
+
+    get($_REQUEST["codCliente"], $_REQUEST["idSession"]);
+?>
