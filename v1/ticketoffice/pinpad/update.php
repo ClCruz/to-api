ea2cccb208286096e58e18a9339849aeae13c3d1
blcoccaro <blcoccaro@gmail.com>
2019-08-09 14:25:07 -0300

    pinpad correction

--- v1/ticketoffice/pinpad/update.php
+++ v1/ticketoffice/pinpad/update.php
@@ -1,10 +1,11 @@
 <?php
     require_once($_SERVER['DOCUMENT_ROOT']."/v1/api_include.php");
 
-    function call($key,$pinpad_acquirerResponseCode,$pinpad_transactionId,$pinpad_executed,$pinpad_error,$pinpad_cancel,$pinpad_ok,$pinpad_fail,$codVenda) {        
-        $query = "EXEC pr_pinpad_update ?,?,?,?,?,?,?,?,?";
+    function call($key,$pinpad_acquirerResponseCode,$pinpad_transactionId,$pinpad_executed,$pinpad_error,$pinpad_cancel,$pinpad_ok,$pinpad_fail,$codVenda,$bin) {        
+        // die("oi");
+        $query = "EXEC pr_pinpad_update ?,?,?,?,?,?,?,?,?,?";
         //key,pinpad_acquirerResponseCode,pinpad_transactionId,pinpad_executed,pinpad_error,pinpad_cancel,pinpad_ok,pinpad_fail
-        $params = array($key,$pinpad_acquirerResponseCode,$pinpad_transactionId,$pinpad_executed,$pinpad_error,$pinpad_cancel,$pinpad_ok,$pinpad_fail,$codVenda);
+        $params = array($key,$pinpad_acquirerResponseCode,$pinpad_transactionId,$pinpad_executed,$pinpad_error,$pinpad_cancel,$pinpad_ok,$pinpad_fail,$codVenda,$bin);
         $result = db_exec($query, $params);
 
         $json = array("success"=>true);
@@ -13,5 +14,5 @@
         logme();
         die();    
     }    
-call($_REQUEST["key"],$_REQUEST["pinpad_acquirerResponseCode"],$_REQUEST["pinpad_transactionId"],$_REQUEST["pinpad_executed"],$_REQUEST["pinpad_error"],$_REQUEST["pinpad_cancel"],$_REQUEST["pinpad_ok"],$_REQUEST["pinpad_fail"],$_REQUEST["codVenda"]);
+call($_REQUEST["key"],$_REQUEST["pinpad_acquirerResponseCode"],$_REQUEST["pinpad_transactionId"],$_REQUEST["pinpad_executed"],$_REQUEST["pinpad_error"],$_REQUEST["pinpad_cancel"],$_REQUEST["pinpad_ok"],$_REQUEST["pinpad_fail"],$_REQUEST["codVenda"],$_REQUEST["bin"]);
 ?>
