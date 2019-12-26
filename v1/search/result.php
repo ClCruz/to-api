7b4ee612cb95925ca83781b8819f39afb71f2a24
Arthur Rodrigues <arthurrg23@gmail.com>
2019-12-26 12:23:35 -0300

    TIC 165

--- v1/search/result.php
+++ v1/search/result.php
@@ -1,70 +1,78 @@
 <?php
-    require_once($_SERVER['DOCUMENT_ROOT']."/v1/api_include.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/v1/api_include.php");
 
-    function search($input, $type, $startAt, $howMany, $api, $city = null, $state = null) {
-        //sleep(5);
-        $query = "EXEC pr_search ?, ?, ?, ?, ?, ?, ?";
-        //die("aaa.".print_r(db_param($startAt),true));
-        $params = array($input, $type, db_param($startAt), db_param($howMany), db_param($city), db_param($state), $api);
-        $result = db_exec($query, $params);
+function search($input, $type, $startAt, $howMany, $api, $city = null, $state = null)
+{
+    //sleep(5);
+    $query = "EXEC pr_search ?, ?, ?, ?, ?, ?, ?";
+    //die("aaa.".print_r(db_param($startAt),true));
+    $params = array($input, $type, db_param($startAt), db_param($howMany), db_param($city), db_param($state), $api);
+    $result = db_exec($query, $params);
 
-        $json = array();
-        foreach ($result as &$row) {
-            $json[] = array(
-                "id_evento" => $row["id_evento"],
-                "ds_evento" => $row["ds_evento"],
-                "cardimage" => getDefaultMediaHost() . str_replace("{id}", $row["id_evento"],str_replace("{default_card}", getDefaultCardImageName(),$row["cardimage"])),
-                "cardbigimage" => getDefaultMediaHost() . str_replace("{id}", $row["id_evento"],str_replace("{default_big}", getDefaultCardImageName(),$row["cardbigimage"])),
-                "description" => $row["description"],
-                "uri" => $row["uri"],
-                "ds_nome_teatro" => $row["ds_local_evento"],//$row["ds_nome_teatro"],
-                "ds_local_evento" => $row["ds_local_evento"],
-                "ds_municipio" => $row["ds_municipio"],
-                "ds_estado" => $row["ds_estado"],
-                "sg_estado" => $row["sg_estado"],
-                "datas" => $row["datas"],
-                "badge"=> splitBadge($row["badges"]),
-                "promo"=> splitPromotion($row["promotion"])
-            );
-        }
-
-        echo json_encode($json);
-        logme();
-        die();    
+    $json = array();
+    foreach ($result as &$row) {
+        $json[] = array(
+            "id_evento" => $row["id_evento"],
+            "ds_evento" => $row["ds_evento"],
+            "cardimage" => getDefaultMediaHost() . str_replace("{id}", $row["id_evento"], str_replace("{default_card}", getDefaultCardImageName(), $row["cardimage"])),
+            "cardbigimage" => getDefaultMediaHost() . str_replace("{id}", $row["id_evento"], str_replace("{default_big}", getDefaultCardImageName(), $row["cardbigimage"])),
+            "description" => $row["description"],
+            "uri" => $row["uri"],
+            "minAmount" => $row["minAmount"],
+            "mmAmountIsPer" => $row["mmAmountIsPer"],
+            "maxAmount" => $row["maxAmount"],
+            "valores" => $row["valores"],
+            "ds_nome_teatro" => $row["ds_local_evento"], //$row["ds_nome_teatro"],
+            "ds_local_evento" => $row["ds_local_evento"],
+            "ds_municipio" => $row["ds_municipio"],
+            "ds_estado" => $row["ds_estado"],
+            "sg_estado" => $row["sg_estado"],
+            "datas" => $row["datas"],
+            "badge" => splitBadge($row["badges"]),
+            "promo" => splitPromotion($row["promotion"])
+        );
     }
 
-    function splitBadge($value) {
-        $ret = array();
+    echo json_encode($json);
+    logme();
+    die();
+}
 
-        if ($value === null)
-            return $ret;
+function splitBadge($value)
+{
+    $ret = array();
 
-        $aux1 = explode(",", $value);
-        foreach ($aux1 as &$row) {
-            $aux2 = explode("|", $row);
-            $aux = array("tag"=>$aux2[0]
-                        ,"img"=>getDefaultMediaHost().$aux2[1]);
-            array_push($ret,$aux);
-        }
-
+    if ($value === null)
         return $ret;
+
+    $aux1 = explode(",", $value);
+    foreach ($aux1 as &$row) {
+        $aux2 = explode("|", $row);
+        $aux = array(
+            "tag" => $aux2[0], "img" => getDefaultMediaHost() . $aux2[1]
+        );
+        array_push($ret, $aux);
     }
-    function splitPromotion($value) {
-        $ret = array();
 
-        if ($value === null)
-            return $ret;
+    return $ret;
+}
+function splitPromotion($value)
+{
+    $ret = array();
 
-        $aux1 = explode(",", $value);
-        foreach ($aux1 as &$row) {
-            $aux2 = explode("|", $row);
-            $aux = array("tag"=>$aux2[1]
-                        ,"img"=>getDefaultMediaHost()."/promo/".$aux2[2]);
-            array_push($ret,$aux);
-        }
-
+    if ($value === null)
         return $ret;
+
+    $aux1 = explode(",", $value);
+    foreach ($aux1 as &$row) {
+        $aux2 = explode("|", $row);
+        $aux = array(
+            "tag" => $aux2[1], "img" => getDefaultMediaHost() . "/promo/" . $aux2[2]
+        );
+        array_push($ret, $aux);
     }
 
-search($_REQUEST["input"], $_REQUEST["type"], $_REQUEST["startat"], $_REQUEST["howmany"], $_REQUEST["apikey"],$_REQUEST["city"],$_REQUEST["state"]);
-?>
+    return $ret;
+}
+
+search($_REQUEST["input"], $_REQUEST["type"], $_REQUEST["startat"], $_REQUEST["howmany"], $_REQUEST["apikey"], $_REQUEST["city"], $_REQUEST["state"]);
