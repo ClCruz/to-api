b50faa7f13ed3518b55c66fcfadb2dd6cedfc534
Arthur Rodrigues <arthurrg23@gmail.com>
2020-03-04 13:36:13 -0300

    teste

--- v1/admin/event/save.php
+++ v1/admin/event/save.php
@@ -38,8 +38,7 @@ function execute(
     $maxAmount,
     $in_entrega_ingresso,
     $external_uri,
-    $mmAmountIsPer,
-    $showPin
+    $mmAmountIsPer
 ) {
     $minAmount = intval(round(($minAmount * 100), 0, PHP_ROUND_HALF_UP));
     $maxAmount = intval(round(($maxAmount * 100), 0, PHP_ROUND_HALF_UP));
@@ -50,7 +49,7 @@ function execute(
 
     $query = "EXEC pr_event_save ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?";
     $params = array(
-        $apikey, $id_produtor, $id_to_admin_user, $CodPeca, $NomPeca, $CodTipPeca, $TemDurPeca, $CenPeca, $id_local_evento, $description, $meta_description, $meta_keyword, $opening_time, $insurance_policy, $showInBanner == "" ? "0" : $showInBanner, $bannerDescription, $QtIngrPorPedido, $in_obriga_cpf == "" ? "0" : ($in_obriga_cpf == "false" || $in_obriga_cpf == false ? "0" : "1"), $qt_ingressos_por_cpf, $ticketoffice_askemail == "" ? "0" : $ticketoffice_askemail, $free_installments, $max_installments, $interest_rate, $ticketoffice_ticketmodel, $showonline, $minAmount, $maxAmount, $in_entrega_ingresso, $external_uri, $mmAmountIsPer, $showPin
+        $apikey, $id_produtor, $id_to_admin_user, $CodPeca, $NomPeca, $CodTipPeca, $TemDurPeca, $CenPeca, $id_local_evento, $description, $meta_description, $meta_keyword, $opening_time, $insurance_policy, $showInBanner == "" ? "0" : $showInBanner, $bannerDescription, $QtIngrPorPedido, $in_obriga_cpf == "" ? "0" : ($in_obriga_cpf == "false" || $in_obriga_cpf == false ? "0" : "1"), $qt_ingressos_por_cpf, $ticketoffice_askemail == "" ? "0" : $ticketoffice_askemail, $free_installments, $max_installments, $interest_rate, $ticketoffice_ticketmodel, $showonline, $minAmount, $maxAmount, $in_entrega_ingresso, $external_uri, $mmAmountIsPer
     );
     $result = db_exec($query, $params, $id_base);
 
@@ -170,6 +169,5 @@ execute(
     $_POST["maxAmount"],
     $_POST["in_entrega_ingresso"],
     $_POST["external_uri"],
-    $_POST["mmAmountIsPer"],
-    $_POST["showPin"]
+    $_POST["mmAmountIsPer"]
 );
