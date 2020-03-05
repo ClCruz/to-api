9b4904e67ef5ee34d656b0a0516228e96ef77829
Arthur Rodrigues <arthurrg23@gmail.com>
2020-03-05 14:17:00 -0300

    teste flag pin

--- v1/admin/event/save.php
+++ v1/admin/event/save.php
@@ -38,7 +38,8 @@ function execute(
     $maxAmount,
     $in_entrega_ingresso,
     $external_uri,
-    $mmAmountIsPer
+    $mmAmountIsPer,
+    $qt_hr_anteced = 0
 ) {
     $minAmount = intval(round(($minAmount * 100), 0, PHP_ROUND_HALF_UP));
     $maxAmount = intval(round(($maxAmount * 100), 0, PHP_ROUND_HALF_UP));
@@ -46,10 +47,17 @@ function execute(
     if ($mmAmountIsPer == 1) {
         $maxAmount = 0;
     }
+    $query = "";
 
-    $query = "EXEC pr_event_save ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?";
+    if ($id_base == 289 || $id_base == 291) {
+        $query = "EXEC pr_event_save ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?";
+    } else {
+        $query = "EXEC pr_event_save ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?";
+    }
+
+    // $query = "EXEC pr_event_save ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?";
     $params = array(
-        $apikey, $id_produtor, $id_to_admin_user, $CodPeca, $NomPeca, $CodTipPeca, $TemDurPeca, $CenPeca, $id_local_evento, $description, $meta_description, $meta_keyword, $opening_time, $insurance_policy, $showInBanner == "" ? "0" : $showInBanner, $bannerDescription, $QtIngrPorPedido, $in_obriga_cpf == "" ? "0" : ($in_obriga_cpf == "false" || $in_obriga_cpf == false ? "0" : "1"), $qt_ingressos_por_cpf, $ticketoffice_askemail == "" ? "0" : $ticketoffice_askemail, $free_installments, $max_installments, $interest_rate, $ticketoffice_ticketmodel, $showonline, $minAmount, $maxAmount, $in_entrega_ingresso, $external_uri, $mmAmountIsPer
+        $apikey, $id_produtor, $id_to_admin_user, $CodPeca, $NomPeca, $CodTipPeca, $TemDurPeca, $CenPeca, $id_local_evento, $description, $meta_description, $meta_keyword, $opening_time, $insurance_policy, $showInBanner == "" ? "0" : $showInBanner, $bannerDescription, $QtIngrPorPedido, $in_obriga_cpf == "" ? "0" : ($in_obriga_cpf == "false" || $in_obriga_cpf == false ? "0" : "1"), $qt_ingressos_por_cpf, $ticketoffice_askemail == "" ? "0" : $ticketoffice_askemail, $free_installments, $max_installments, $interest_rate, $ticketoffice_ticketmodel, $showonline, $minAmount, $maxAmount, $in_entrega_ingresso, $external_uri, $mmAmountIsPer, $qt_hr_anteced
     );
     $result = db_exec($query, $params, $id_base);
 
@@ -169,5 +177,6 @@ execute(
     $_POST["maxAmount"],
     $_POST["in_entrega_ingresso"],
     $_POST["external_uri"],
-    $_POST["mmAmountIsPer"]
+    $_POST["mmAmountIsPer"],
+    $_POST["qt_hr_anteced"]
 );
